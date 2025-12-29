be8e51812aa95f768a4dee363de97b29d8e996f4
Pratibha Jain <pratibha@example.com>
2025-12-29 16:27:38 +0530

    ner

--- src/screens/banner/banner.jsx
+++ src/screens/banner/banner.jsx
@@ -16,7 +16,7 @@ const Banner = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000
+        autoplaySpeed: 3000
     };
 
     return (
